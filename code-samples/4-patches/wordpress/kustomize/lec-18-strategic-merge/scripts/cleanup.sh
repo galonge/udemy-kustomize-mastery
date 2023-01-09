8dcd26d4a6fc07e7b4b3a2ac46ba03f79bd393e9
George Alonge <georgedelson@gmail.com>
2023-01-08 22:46:15 -0800

    sec 4: working with patches

---
+++ code-samples/4-patches/wordpress/kustomize/lec-18-strategic-merge/scripts/cleanup.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# Delete all created resources in the namespace
+kubectl delete namespace lec-18
+
+# Delete the results.yml file if exist
+rm -f results.yml
+
+# Optional - if using k3d 
+# k3d cluster delete kustomize-test
